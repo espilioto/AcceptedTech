28258878915b975ec680dec64380fa561b68b539
unknown <espilioto@gmail.com>
2024-01-31 13:45:20 +0200

    Commands

---
+++ acceptedTech.Application/Matches/Commands/CreateMatch/CreateMatchCommand.cs
@@ -0,0 +1,13 @@
+﻿using acceptedTech.Domain.Enums;
+using MediatR;
+
+namespace acceptedTech.Application.Matches.Commands.CreateMatch
+{
+    public record CreateMatchCommand(
+            string Description,
+            DateOnly MatchDate,
+            TimeOnly MatchTime,
+            string TeamA,
+            string TeamB,
+            int Sport) : IRequest<int>;
+}
---
+++ acceptedTech.Application/Matches/Commands/CreateMatch/CreateMatchCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using acceptedTech.Domain.Enums;
+using MediatR;
+
+namespace acceptedTech.Application.Matches.Commands.CreateMatch
+{
+    public class CreateMatchCommandHandler : IRequestHandler<CreateMatchCommand, int>
+    {
+        public Task<int> Handle(CreateMatchCommand request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(int.MaxValue);
+        }
+    }
+}
